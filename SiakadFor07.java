5e17424dea44475cb8f8b705139a902e43297304
FataRach <emillulfr@gmail.com>
2025-10-24 14:51:18 +0700

    After Modification

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -5,6 +5,7 @@ public class SiakadFor07 {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         // Perulangan untuk 10 mahasiswa
         for (int i = 1; i <= 10; i++) {
@@ -16,10 +17,18 @@ public class SiakadFor07 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+        // Menghitung banyaknya jumlah mahasiswa lulus/tidak lulus
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
 
         System.out.println("\nNilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
-        
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
     }
-}
+  }
+  
